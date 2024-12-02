99f72f14ad2f934c693bdd21111d43b8439a0a5f
blue <blue.4e2@gmail.com>
2024-12-02 18:26:16 +0530

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -184,7 +184,7 @@ const Cart = () => {
               <Loading/>
           </div>
         }
-        <Link to={`/`} className='w-11/12 flex justify-start py-3'>
+        <Link to={`/dashboard`} className='w-11/12 flex justify-start py-3'>
           <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 cursor-pointer' viewBox="0 0 32 32"><path d="M32 15H3.41l8.29-8.29-1.41-1.42-10 10a1 1 0 0 0 0 1.41l10 10 1.41-1.41L3.41 17H32z" data-name="4-Arrow Left"/></svg>
         </Link>
         <div className='w-full z-40 flex flex-col items-end fixed px-3 right-1 md:right-4 top-14'>
